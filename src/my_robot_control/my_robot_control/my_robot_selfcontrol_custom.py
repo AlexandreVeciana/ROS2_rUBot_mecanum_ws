529bda80552e2c19e1b31790636a81ea429937d9
arnaugene <agenegar65@alumnes.ub.edu>
2025-11-22 18:08:43 +0000

    millores

--- src/my_robot_control/my_robot_control/my_robot_selfcontrol_custom.py
+++ src/my_robot_control/my_robot_control/my_robot_selfcontrol_custom.py
@@ -11,7 +11,7 @@ class RobotSelfControl(Node):
         super().__init__('robot_selfcontrol_node')
 
         # Configurable parameters
-        self.declare_parameter('distance_laser', 0.2)
+        self.declare_parameter('distance_laser', 0.3)
         self.declare_parameter('speed_factor', 1.0)
         self.declare_parameter('forward_speed', 0.2)
         self.declare_parameter('rotation_speed', 0.3)
@@ -25,6 +25,7 @@ class RobotSelfControl(Node):
 
         self._msg = Twist()
         self._msg.linear.x = self._forwardSpeed * self._speedFactor
+        self._msg.linear.y = 0.0
         self._msg.angular.z = 0.0
 
         self._cmdVel = self.create_publisher(Twist, '/cmd_vel', 10)
@@ -52,7 +53,7 @@ class RobotSelfControl(Node):
         self._cmdVel.publish(self._msg)
 
         if now_sec - self._last_speed_time >= 1:
-            self.get_logger().info(f"Vx: {self._msg.linear.x:.2f} m/s, w: {self._msg.angular.z:.2f} rad/s | Time: {elapsed_time:.1f}s")
+            self.get_logger().info(f"Vx: {self._msg.linear.x:.2f} m/s, Vy: {self._msg.linear.y:.2f} m/s, w: {self._msg.angular.z:.2f} rad/s | Time: {elapsed_time:.1f}s")
             self._last_speed_time = now_sec
 
         if elapsed_time >= self._time_to_stop:
@@ -82,23 +83,22 @@ class RobotSelfControl(Node):
         angle_closest_deg = angle_min_deg + element_index * angle_increment_deg
 
         # Determine zone
-        if -25 <= angle_closest_deg <= 25:
+        if -35 <= angle_closest_deg <= 35:
             zone = "FRONT"
-        elif 65 < angle_closest_deg <= 115:
+        elif 55 < angle_closest_deg <= 125:
             zone = "LEFT"
-        elif -115 <= angle_closest_deg < -65:
+        elif -125 <= angle_closest_deg < -55:
             zone = "RIGHT"
-        elif 155 <= angle_closest_deg < -155:
-            zone = "BACK"
-        elif 115 < angle_closest_deg <= 155:
+        elif 125 < angle_closest_deg <= 145:
             zone = "BACK_LEFT"
-        elif -155 <= angle_closest_deg < -115:
+        elif -145 <= angle_closest_deg < -125:
             zone = "BACK_RIGHT"
-        elif 25 < angle_closest_deg <= 65:
+        elif 35 < angle_closest_deg <= 55:
             zone = "FRONT_LEFT"
-        elif -65 <= angle_closest_deg < -25:
+        elif -55 <= angle_closest_deg < -35:
             zone = "FRONT_RIGHT"
-
+        elif angle_closest_deg > 145 or angle_closest_deg < -145:
+            zone = "BACK"
         else:
             zone = "OUTSIDE FOV"
 
@@ -140,10 +140,6 @@ class RobotSelfControl(Node):
             elif zone == "BACK":
                 self._msg.linear.x = 0.0
                 self._msg.linear.y = -self._forwardSpeed * self._speedFactor
-                self._msg.angular.z = 0.0
-            else:
-                self._msg.linear.x = self._forwardSpeed * self._speedFactor
-                self._msg.linear.y = 0.0
                 self._msg.angular.z = 0.0
         else:
             self._msg.linear.x = self._forwardSpeed * self._speedFactor
